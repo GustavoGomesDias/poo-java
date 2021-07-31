e430a9c1295d79f7731bd6b97843de1dff3cf811
GustavoGomesDias <treeelevengg@gmail.com>
2021-07-31 02:51:56 -0300

    feat: add desserealizaDat

--- gerenciaViagem2/src/aplicacaoDat/AplicacaoDat.java
+++ gerenciaViagem2/src/aplicacaoDat/AplicacaoDat.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.IOException;
@@ -17,6 +18,7 @@ public class AplicacaoDat {
     public static void main(String[] args) {
         AplicacaoDat.setVendedores(args);
         AplicacaoDat.serealizaDat();
+        AplicacaoDat.desserealizaDat();
     }
 
     public static void serealizaDat() {
@@ -27,17 +29,36 @@ public class AplicacaoDat {
             obj.writeObject(AplicacaoDat.vendedores);
             obj.close();
             System.out.println("Objeto gravado com sucesso!");
+            AplicacaoDat.vendedores.clear();
         } catch (IOException error) {
             System.out.println(error.getMessage());
         }
     }
 
+    public static void desserealizaDat() {
+        try {
+            File arq = new File("vendedores.dat");
+            if (arq.exists()) {
+                FileInputStream inputArq = new FileInputStream(arq);
+                ObjectInputStream objArq = new ObjectInputStream(inputArq);
+                AplicacaoDat.vendedores = (ArrayList<Vendedor>) objArq.readObject();
+                AplicacaoDat.relatorio();
+            } else {
+                System.out.println("Arquivo não existe!");
+            }
+
+        } catch (IOException | ClassNotFoundException error) {
+            System.out.println(error.getMessage());
+        }
+    }
+
     public static void setVendedores(String[] args) {
-        Scanner read = new Scanner(System.in).useDelimiter("\n");
+        Scanner read = new Scanner(System.in);
+        read.useDelimiter("\n");
 
         String nome;
 
-        nome = read.nextLine();
+        nome = read.next();
         while(!nome.equalsIgnoreCase("fim")) {
             Vendedor vendedor = new Vendedor();
             vendedor.setNome(nome);
@@ -48,7 +69,7 @@ public class AplicacaoDat {
             km = read.nextInt();
 
             while(km != 0) {
-                descricao = read.nextLine();
+                descricao = read.next();
                 dias = read.nextInt();
                 vendas = read.nextFloat();
 
@@ -62,11 +83,21 @@ public class AplicacaoDat {
                 km = read.nextInt();
             }
             AplicacaoDat.loadVedendor(vendedor);
-            nome = read.nextLine();
+            nome = read.next();
         }
     }
 
     public static void loadVedendor(Vendedor vendedor) {
         AplicacaoDat.vendedores.add(vendedor);
+    }
+
+    public static void relatorio () {
+        if (!AplicacaoDat.vendedores.isEmpty()) {
+            for (Vendedor vendedor : AplicacaoDat.vendedores) {
+                System.out.println(vendedor.getNome());
+            }
+        } else {
+            System.out.println("Lista vazia!");
+        }
     }
 }

9605bc5ce7abd7fd2b50f2e4f91cc9e4d2d5d018
GustavoGomesDias <treeelevengg@gmail.com>
2021-08-04 23:07:12 -0300

    feat: add array

--- gerenciaViagem2/src/manipulaArq/ManipuladorArq.java
+++ gerenciaViagem2/src/manipulaArq/ManipuladorArq.java
@@ -1,8 +1,16 @@
 package manipulaArq;
 
 import java.io.*;
+import java.util.ArrayList;
+
+import dominio.Vendedor;
+import dominio.Viagem;
+
+import static java.lang.Float.parseFloat;
+import static java.lang.Integer.parseInt;
 
 public class ManipuladorArq {
+    private ArrayList<Vendedor> vendedores = new ArrayList<Vendedor>();
     public String inputFile;
     public String outputFile;
     public String path;
@@ -13,18 +21,45 @@ public class ManipuladorArq {
         this.outputFile = outputFile + ".txt";
     }
 
+    public boolean fileExists(String option) throws IOException {
+        File file = new File(this.path + "/" + option +"/" + this.inputFile);
+        return file.exists();
+    }
+
     public void leitor() {
         try {
             if (!this.fileExists("input")) {
                 System.out.println("Arquivo de entrada não existente!");
                 return;
             }
-            String linha = "";
+
             BufferedReader bufferedReader = new BufferedReader(new FileReader(this.path + "/input/" + this.inputFile));
-            linha = bufferedReader.readLine();
-            while(linha != null) {
-                System.out.println(linha.getClass().getSimpleName() + ": " + linha);
-                linha = bufferedReader.readLine();
+            String nome = "";
+            nome = bufferedReader.readLine();
+            while(!nome.equalsIgnoreCase("fim")) {
+                Vendedor vendedor = new Vendedor();
+                vendedor.setNome(nome);
+                int km;
+                String descricao;
+                int dias;
+                float vendas;
+
+                km = parseInt(bufferedReader.readLine());
+                while (km != 0) {
+                    descricao = bufferedReader.readLine();
+                    dias = parseInt(bufferedReader.readLine());
+                    vendas = parseFloat(bufferedReader.readLine());
+
+                    Viagem viagem = new Viagem();
+                    viagem.setKm(km);
+                    viagem.setDescricao(descricao);
+                    viagem.setDias(dias);
+                    viagem.setValorVendas(vendas);
+                    vendedor.loadViagem(viagem);
+                    km = parseInt(bufferedReader.readLine());
+                }
+                this.vendedores.add(vendedor);
+                nome = bufferedReader.readLine();
             }
             bufferedReader.close();
         } catch(IOException error) {
@@ -32,8 +67,7 @@ public class ManipuladorArq {
         }
     }
 
-    public boolean fileExists(String option) throws IOException {
-        File file = new File(this.path + "/" + option +"/" + this.inputFile);
-        return file.exists();
+    public ArrayList<Vendedor> getVendedores() {
+        return this.vendedores;
     }
 }
